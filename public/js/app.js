121dc40b4d1d4f856b091f68f9f2657834b5564c
Brett <brett_matsumoto@yahoo.com>
2019-04-12 16:39:52 -1000

    added comments and sections for different matchMedias and selected

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,47 @@
 'use strict';
 
-if (window.matchMedia('(min-width: 320px)').matches) {
-  const rSlashJavaScript = new XMLHttpRequest();
-  rSlashJavaScript.addEventListener('load', reqListenerJavaScript);
-  rSlashJavaScript.open('GET', 'https://www.reddit.com/r/javascript/.json');
-  rSlashJavaScript.send();
+let selected = 'javascript';
 
-  function reqListenerJavaScript() {
-    console.log(JSON.parse(this.responseText).data.children);
+if (selected = 'javascript'){ //if r/JavaScript is selected;
 
-    const responseText = JSON.parse(this.responseText).data.children;
-    for (let i = 0; i < responseText.length; i++) {
-      let jSTitleDiv = document.createElement('div');
-      jSTitleDiv.innerHTML = responseText[i].data.title;
-      jSTitleDiv.className = 'contentBox';
-      let contentBox = document.getElementById('content');
-      contentBox.appendChild(jSTitleDiv);
-
-      let jSContentP = document.createElement('p');
-      jSContentP.innerHTML = responseText[i].data.selftext;
-      jSTitleDiv.appendChild(jSContentP);
+  if (window.matchMedia('(min-width: 320px)').matches) { //Javascript for iphone sized devices;
+    const rSlashJavaScript = new XMLHttpRequest();
+    rSlashJavaScript.addEventListener('load', reqListenerJavaScript);
+    rSlashJavaScript.open('GET', 'https://www.reddit.com/r/javascript/.json');
+    rSlashJavaScript.send();
+  
+    function reqListenerJavaScript() {
+      console.log(JSON.parse(this.responseText).data.children);
+  
+      const responseText = JSON.parse(this.responseText).data.children;
+      for (let i = 0; i < responseText.length; i++) {
+        let jSTitleDiv = document.createElement('div');
+        jSTitleDiv.innerHTML = responseText[i].data.title;
+        jSTitleDiv.className = 'contentBox';
+        let contentBox = document.getElementById('content');
+        contentBox.appendChild(jSTitleDiv);
+  
+        let jSContentP = document.createElement('p');
+        jSContentP.innerHTML = responseText[i].data.selftext;
+        jSTitleDiv.appendChild(jSContentP);
+      }
     }
   }
+  if (window.matchMedia('(min-width: 768px)').matches) { //Javascript for tablet sized devices;
+  }
+  if (window.matchMedia('(min-width: 1224px').matches) { //Javascript for desktop/laptop sized devices;
+  }
+} 
+
+if (selected = 'choosingbeggers'){ // if choosingbeggers is selected;
+
 }
-if (window.matchMedia('(min-width: 768px)').matches) {
+
+if (selected = 'whatcouldgowrong'){ // if whatcouldgowrong is selected;
+
 }
-if (window.matchMedia('(min-width: 1224px').matches) {
+
+if (selected = 'random'){ // if random is selected;
+
 }
+
