882fedab3129789613eb6a29a7cae7950b102058
Brett <brett_matsumoto@yahoo.com>
2019-04-12 19:25:10 -1000

    added scores

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,21 @@
 'use strict';
 
-let selected = 'javascript';
+let selected = 'r/JavaScript';
 
-if (selected = 'javascript'){ //if r/JavaScript is selected;
+if (selected === 'r/JavaScript') {
+  //if r/JavaScript is selected;
 
-  if (window.matchMedia('(min-width: 320px)').matches) { //Javascript for iphone sized devices;
+  if (window.matchMedia('(min-width: 320px)').matches) {
+    //Javascript for iphone sized devices;
+
     const rSlashJavaScript = new XMLHttpRequest();
     rSlashJavaScript.addEventListener('load', reqListenerJavaScript);
     rSlashJavaScript.open('GET', 'https://www.reddit.com/r/javascript/.json');
     rSlashJavaScript.send();
-  
+
     function reqListenerJavaScript() {
       console.log(JSON.parse(this.responseText).data.children);
-  
+
       const responseText = JSON.parse(this.responseText).data.children;
       for (let i = 0; i < responseText.length; i++) {
         let jSTitleDiv = document.createElement('div');
@@ -20,28 +23,49 @@ if (selected = 'javascript'){ //if r/JavaScript is selected;
         jSTitleDiv.className = 'contentBox';
         let contentBox = document.getElementById('content');
         contentBox.appendChild(jSTitleDiv);
-  
+
         let jSContentP = document.createElement('p');
         jSContentP.innerHTML = responseText[i].data.selftext;
         jSTitleDiv.appendChild(jSContentP);
+        
+        let jSLinks = document.createElement('p');
+        jSLinks.innerHTML = responseText[i].data.url;
+        jSContentP.appendChild(jSLinks);
+        
+        let jSScores = document.createElement('span');
+        jSScores.innerHTML = 'score:' + responseText[i].data.score;
+        jSContentP.appendChild(jSScores);
       }
     }
-  }
-  if (window.matchMedia('(min-width: 768px)').matches) { //Javascript for tablet sized devices;
-  }
-  if (window.matchMedia('(min-width: 1224px').matches) { //Javascript for desktop/laptop sized devices;
-  }
-} 
 
-if (selected = 'choosingbeggers'){ // if choosingbeggers is selected;
+    let navigationButtons = document.getElementsByClassName('navButtons');
+    for (let i = 0; i < navigationButtons.length; i++) {
+      navigationButtons[i].addEventListener('click', swapSelected);
+    }
 
+    function swapSelected() {
+      selected = this.innerHTML;
+      console.log(selected);
+    }
+  }
+
+  if (window.matchMedia('(min-width: 768px)').matches) {
+    //Javascript for tablet sized devices;
+  }
+
+  if (window.matchMedia('(min-width: 1224px').matches) {
+    //Javascript for desktop/laptop sized devices;
+  }
 }
 
-if (selected = 'whatcouldgowrong'){ // if whatcouldgowrong is selected;
-
+if (selected === 'r/ChoosingBeggers') {
+  // if choosingbeggers is selected;
 }
 
-if (selected = 'random'){ // if random is selected;
-
+if (selected === 'r/WhatCouldGoWrong') {
+  // if whatcouldgowrong is selected;
 }
 
+if (selected === 'RANDOM') {
+  // if random is selected;
+}
