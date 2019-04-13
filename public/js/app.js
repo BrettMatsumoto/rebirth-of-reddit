77466b21685177bee7eee43a51df39bf33fea59d
Brett <brett_matsumoto@yahoo.com>
2019-04-13 12:31:58 -1000

    worked in more subreddits

--- public/js/app.js
+++ public/js/app.js
@@ -1,71 +1,117 @@
 'use strict';
 
-let selected = 'r/JavaScript';
-
-if (selected === 'r/JavaScript') {
-  //if r/JavaScript is selected;
-
-  if (window.matchMedia('(min-width: 320px)').matches) {
-    //Javascript for iphone sized devices;
-
-    const rSlashJavaScript = new XMLHttpRequest();
-    rSlashJavaScript.addEventListener('load', reqListenerJavaScript);
-    rSlashJavaScript.open('GET', 'https://www.reddit.com/r/javascript/.json');
-    rSlashJavaScript.send();
-
-    function reqListenerJavaScript() {
-      console.log(JSON.parse(this.responseText).data.children);
-
-      const responseText = JSON.parse(this.responseText).data.children;
-      for (let i = 0; i < responseText.length; i++) {
-        let jSTitleDiv = document.createElement('div');
-        jSTitleDiv.innerHTML = responseText[i].data.title;
-        jSTitleDiv.className = 'contentBox';
-        let contentBox = document.getElementById('content');
-        contentBox.appendChild(jSTitleDiv);
-
-        let jSContentP = document.createElement('p');
-        jSContentP.innerHTML = responseText[i].data.selftext;
-        jSTitleDiv.appendChild(jSContentP);
-        
-        let jSLinks = document.createElement('p');
-        jSLinks.innerHTML = responseText[i].data.url;
-        jSContentP.appendChild(jSLinks);
-        
-        let jSScores = document.createElement('span');
-        jSScores.innerHTML = 'score:' + responseText[i].data.score;
-        jSContentP.appendChild(jSScores);
-      }
-    }
-
-    let navigationButtons = document.getElementsByClassName('navButtons');
-    for (let i = 0; i < navigationButtons.length; i++) {
-      navigationButtons[i].addEventListener('click', swapSelected);
-    }
-
-    function swapSelected() {
-      selected = this.innerHTML;
-      console.log(selected);
-    }
-  }
-
-  if (window.matchMedia('(min-width: 768px)').matches) {
-    //Javascript for tablet sized devices;
-  }
-
-  if (window.matchMedia('(min-width: 1224px').matches) {
-    //Javascript for desktop/laptop sized devices;
-  }
+let selected = 'r/WhatCouldGoWrong';
+console.log(selected);
+let navigationButtons = document.getElementsByClassName('navButtons');
+for (let i = 0; i < navigationButtons.length; i++) {
+  navigationButtons[i].addEventListener('click', swapSelected);
 }
 
-if (selected === 'r/ChoosingBeggers') {
-  // if choosingbeggers is selected;
+function swapSelected() {
+  selected = this.innerHTML;
+  console.log(selected);
 }
 
-if (selected === 'r/WhatCouldGoWrong') {
-  // if whatcouldgowrong is selected;
-}
+// if (selected === 'r/JavaScript') {
+
+//   const rSlashJavaScript = new XMLHttpRequest();
+//   rSlashJavaScript.addEventListener('load', reqListenerJavaScript);
+//   rSlashJavaScript.open('GET', 'https://www.reddit.com/r/javascript/.json');
+//   rSlashJavaScript.send();
+
+//   function reqListenerJavaScript() {
+//     const responseText = JSON.parse(this.responseText).data.children;
+
+//     for (let i = 0; i < responseText.length; i++) {
+//       let jSTitleDiv = document.createElement('div');
+//       jSTitleDiv.innerHTML = responseText[i].data.title;
+//       jSTitleDiv.className = 'jSContentBox';
+//       let contentBox = document.getElementById('content');
+//       contentBox.appendChild(jSTitleDiv);
+
+//       let jSContentP = document.createElement('p');
+//       jSContentP.innerHTML = responseText[i].data.selftext;
+//       jSTitleDiv.appendChild(jSContentP);
+
+//       let jSLinks = document.createElement('p');
+//       jSLinks.innerHTML = responseText[i].data.url;
+//       jSContentP.appendChild(jSLinks);
+//     }
+//   }
+// }
+
+// if (selected === 'r/ChoosingBeggers') {
+//   const rSlashChoosingBeggers = new XMLHttpRequest();
+//   rSlashChoosingBeggers.addEventListener('load', reqListenerChoosingBeggers);
+//   rSlashChoosingBeggers.open('GET', 'https://www.reddit.com/r/ChoosingBeggars/.json');
+//   rSlashChoosingBeggers.send();
+
+//   function reqListenerChoosingBeggers() {
+//     const responseText = JSON.parse(this.responseText).data.children;
+
+//     for (let i = 0; i < responseText.length; i++) {
+//       let choosingBeggersTitleDiv = document.createElement('div');
+//       choosingBeggersTitleDiv.innerHTML = responseText[i].data.title;
+//       choosingBeggersTitleDiv.className = 'cBContentBox';
+//       let contentBox = document.getElementById('content');
+//       contentBox.appendChild(choosingBeggersTitleDiv);
+
+//       let choosingBeggersContent = document.createElement('p');
+//       choosingBeggersContent.innerHTML = responseText[i].data.selftext;
+//       choosingBeggersTitleDiv.appendChild(choosingBeggersContent);
+
+//       let choosingBeggersPictures = document.createElement('img');
+//       choosingBeggersPictures.src = responseText[i].data.thumbnail;
+//       choosingBeggersContent.appendChild(choosingBeggersPictures);
+//       console.log(choosingBeggersPictures)
+//     }
+//   }
+// }
+
+// if (selected === 'r/WhatCouldGoWrong') {
+//   const rSlashWhatCouldGoWrong = new XMLHttpRequest();
+//   rSlashWhatCouldGoWrong.addEventListener('load', reqListenerWCGW);
+//   rSlashWhatCouldGoWrong.open('GET', 'https://www.reddit.com/r/Whatcouldgowrong/.json');
+//   rSlashWhatCouldGoWrong.send();
+
+//   function reqListenerWCGW() {
+//     const responseText = JSON.parse(this.responseText).data.children;
+
+//     for (let i = 0; i < responseText.length; i++) {
+//       let whatCouldGoWrongTitleDiv = document.createElement('div');
+//       whatCouldGoWrongTitleDiv.innerHTML = responseText[i].data.title;
+//       whatCouldGoWrongTitleDiv.className = 'WCGWBox';
+//       let contentBox = document.getElementById('content');
+//       contentBox.appendChild(whatCouldGoWrongTitleDiv);
+
+//       let whatCouldGoWrongLink = document.createElement('a');
+//       whatCouldGoWrongLink.href = responseText[i].data.url;
+//       whatCouldGoWrongLink.innerHTML = responseText[i].data.url;
+//       whatCouldGoWrongLink.target = '_blank';
+//       console.log(whatCouldGoWrongLink.href);
+//       whatCouldGoWrongTitleDiv.appendChild(whatCouldGoWrongLink);
+//     }
+//   }
+// }
 
 if (selected === 'RANDOM') {
-  // if random is selected;
-}
+  let subRandomChoices = ['https://www.reddit.com/r/DnD/.json', 'https://www.reddit.com/r/entitledparents/.json', 'https://www.reddit.com/r/dankmemes/.json', 'https://www.reddit.com/r/NEVERGONNAGIVEYOUUP/.json', 'https://www.reddit.com/r/CrappyDesign/.json', 'https://www.reddit.com/r/nailedit/.json', 'https://www.reddit.com/r/whoosh/.json'];
+  let randomNumberGenerator = Math.floor(Math.random()*7);
+
+  const rSlashRandom = new XMLHttpRequest();
+  rSlashRandom.addEventListener('load', reqListenerRandom);
+  rSlashRandom.open('GET', subRandomChoices[randomNumberGenerator]);
+  rSlashRandom.send();
+
+  function reqListenerRandom(){
+    const responseText = JSON.parse(this.responseText).data.children;
+
+    for (let i = 0; i < responseText.lengthl; i++){
+      let randomTitleDiv = document.createElement('div');
+      randomTitleDiv.innerHTML = responseText[i].data.title;
+      randomTitleDiv.className = 'randomBox';
+      let contentBox = document.getElementById('content');
+      contentBox.appendChild(randomTitleDiv);
+    }
+  }
+  }
